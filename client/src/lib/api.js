b26ab6cad442777edbd4802845beb3fa9552a557
Y Abhinav <abhinavysns03@gmail.com>
2026-03-31 14:37:43 +0530

    "Samll change in baseUrl"

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL:"https://travelnest-ideaverse-0xpt.onrender.com",
 });
 
 async function buildAuthHeaders(getToken) {
